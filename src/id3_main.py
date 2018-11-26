373cf4614aa4156a5909cdce4244875238ff002c
u3556177 <u3556177@connect.hku.hk>
2018-11-27 02:27:03 +0800

    Added time measurement

--- src/id3_main.py
+++ src/id3_main.py
@@ -4,6 +4,7 @@
 ############### Breast cancer classification by ID3 ##################
 
 import utils
+import datetime
 import id3
 from graphviz import Source
 
@@ -11,6 +12,9 @@ if __name__ == '__main__':
     
     # load dataset
     X_train, X_test, y_train, y_test, feature_labels = utils.loadDataSet()
+
+    # start timer
+    startTime = datetime.datetime.now()
     
     # init Id3Estimator
     model = id3.Id3Estimator()
@@ -20,6 +24,11 @@ if __name__ == '__main__':
 
     # make predictions
     y_pred = model.predict(X_test)
+
+    # end timer
+    endTime = datetime.datetime.now()
+    timeElapsed = endTime - startTime
+    print('Total elapsed time for ID3 = ' + str(round(timeElapsed.total_seconds() * 1000, 4)) + ' ms')
 
     # evaluate predictions
     utils.printAccuarcy(y_test, y_pred)
--- src/xgb_main.py
+++ src/xgb_main.py
@@ -4,6 +4,7 @@
 ############### Breast cancer classification by XGBoost ##################
 
 import utils
+import datetime
 import xgboost as xgb
 from matplotlib.pyplot import show
 
@@ -12,14 +13,23 @@ if __name__ == '__main__':
     # load dataset
     X_train, X_test, y_train, y_test, feature_labels = utils.loadDataSet()
 
+    # start timer
+    startTime = datetime.datetime.now()
+
     # fit model
     train_data = xgb.DMatrix(X_train, y_train, feature_names=feature_labels)
-    train_param = {'max_depth':5, 'eta':0.1, 'silent':0, 'objective':'binary:logistic'}
-    model = xgb.train(train_param, train_data)
+    train_param = {'max_depth':5, 'eta':0.1, 'silent':1, 'objective':'binary:logistic'}
+    train_iteration = 100
+    model = xgb.train(train_param, train_data, train_iteration)
 
     # make predictions for test data
     test_data = xgb.DMatrix(X_test, y_test, feature_names=feature_labels)
     y_pred = model.predict(test_data)
+
+    # end timer
+    endTime = datetime.datetime.now()
+    timeElapsed = endTime - startTime
+    print('Total elapsed time for XGBoost ' + str(train_iteration) + ' training iterations = ' + str(round(timeElapsed.total_seconds() * 1000, 4)) + ' ms')
 
     # evaluate predictions
     utils.printAccuarcy(y_test, y_pred)
